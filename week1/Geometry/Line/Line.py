ee02692db40cda684e417d7684a3e8a65e471963
cdchavezq@unal.edu.co <cdchavezq@unal.edu.co>
2022-08-24 14:43:39 -0300

    ended implementation of line geometry element

--- week1/Geometry/Line/Line.py
+++ week1/Geometry/Line/Line.py
@@ -26,48 +26,98 @@ class Line:
             raise ValueError("[error] a second point or a slope is needed to form a line")
         
         else:
-            pass
+            self.p1 = p1
+            self.p2 = p2
+            self.slope = slope
+            self.y_intersection = y_intersection
         
 
-    def is_vertical(self)->boolean:
-        if self._p1 and self._p2:
-            return (self._p1.x_coordinate == self._p2.x_coordinate) or (self.slope == float('inf'))
-        
-            
 
     @property
     def slope(self)->float:
-        return self._slope
+        if self._slope:
+            return self._slope
+        elif self.p1 and self.p2:
+            return (self.p2.y_coordinate - self.p1.y_coordinate)/(self.p2.x_coordinate - self.p2.x_coordinate)
+        
+        elif (self.p1 or self.p2) and self.y_intersection:            
+            point:Point = list(filter(lambda x : x != None,[self.p1,self.p2]))[0]
+            if point.x_coordinate == 0:
+                raise ValueError("[error] provided point can't have x-coordinate=0")
+            return (point.y_coordinate - self.y_intersection)/point.x_coordinate
+
+        else:
+            raise ValueError("[error] slope can not be determined")
     
     @slope.setter
-    def slope(self,slope_value:float,y_intersection:float = None,p1_value:Point=None,p2_value:Point=None)->None:
-        pass
+    def slope(self,slope_value:float)->None:
+        if not self._slope:
+            raise ValueError("Slope was already provided")
+        elif (slope_value == float('inf')) and not (self.p1 or self.p2):
+            raise ValueError("With an infinite slope at least one point must be provided")
+        elif (slope_value == 0) and not (self.p1 or self.p2):
+            raise ValueError("With a zero slope at least one point must be provided")
+        else:
+            self._slope = slope_value
     
+
+
     @property
     def y_intersection(self)->float:
-        return self._y_intersection
+        if self._y_intersection:
+            return self._y_intersection
+        elif (self.p1 or self.p2) and self.slope:
+            point:Point = list(filter(lambda x : x != None,[self.p1,self.p2]))[0]
+            return point.y_coordinate - self.slope*point.x_coordinate
     
     @y_intersection.setter
-    def y_intersection(self,value)->None:
-        self._y_intersection = value
+    def y_intersection(self,y_intersection_value)->None:
+        if not self._y_intersection:            
+            raise ValueError("y-intersection was already provided")
+        else:
+            print(f"[info] {y_intersection_value} is set as y-intersection")
+            self._y_intersection = y_intersection_value
 
 
     @property
-    def p1(self):
+    def p1(self)->Point:
         return self._p1
     
     @p1.setter
-    def p1(self,p1_value):
-        print(f"[info] {p1_value} is a point of the line")
-        self._p1 = p1_value
+    def p1(self,p1_value)->None:
+        if not self._p1:
+            raise ValueError("p1 point was already provided")
+        else:
+            print(f"[info] {p1_value} is a point of the line")
+            self._p1 = p1_value
     
     @property
-    def p2(self):
+    def p2(self)->Point:
         return self._p2
     
     @p2.setter
-    def p2(self,p2_value):
-        print(f"[info] {p2_value} is a point of the line")
-        self._p2 = p2_value
+    def p2(self,p2_value)->None:
+        if not self._p2:
+            raise ValueError("p2 point was already provided")
+        else:
+            print(f"[info] {p2_value} is a point of the line")
+            self._p2 = p2_value
 
+
+    def is_vertical(self)->boolean:
+        if self.p1 and self.p2:
+            return (self.p1.x_coordinate == self.p2.x_coordinate)
+        elif self.slope:
+            return (self.slope == float('inf'))
+        else:            
+            raise ValueError("[error] can not be determined verticality")
+    
+    def is_horizontal(self)->boolean:        
+        if self.p1 and self.p2:
+            return (self.p1.y_coordinate == self.p2.y_coordinate)
+        elif self.slope:
+            return (self.slope == 0)
+        else:
+            raise ValueError("[error] can not be determined verticality")
+    
     
