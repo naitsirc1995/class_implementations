a0cdff23a3e4d49feff0a49fdf8c65b3b71885d7
cdchavezq@unal.edu.co <cdchavezq@unal.edu.co>
2022-09-09 00:16:56 -0300

    removed some unnecessary comments

--- week3/grapher/graph.cpp
+++ week3/grapher/graph.cpp
@@ -19,8 +19,6 @@ void graphPolygon(Polygon* polygon)
     for (int i = 0; i < numberOfPoints; i++)
     {        
         PolygonVertex* currentVertex = &polygonVertices[i];
-        std::cout << "x-coordinates: " <<  currentVertex->point.x_coordinate << " " << currentVertex->next->point.x_coordinate << std::endl;
-        std::cout << "y-coordinates: " <<  currentVertex->point.y_coordinate << " " << currentVertex->next->point.y_coordinate << std::endl;
         std::vector<float> x = {currentVertex->point.x_coordinate,currentVertex->next->point.x_coordinate};
         std::vector<float> y = {currentVertex->point.y_coordinate,currentVertex->next->point.y_coordinate};
         plt::plot(x,y);
